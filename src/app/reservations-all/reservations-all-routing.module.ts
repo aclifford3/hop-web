560ef4ce9ea1698e39e10143c34c19553dd5439d
Alec Clifford <alec.clifford@gmail.com>
2017-12-15 09:30:53 -0500

    Add all reservations module

--- src/app/reservations-all/reservations-all-routing.module.ts
+++ src/app/reservations-all/reservations-all-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { extract } from '../core/i18n.service';
 import {ReservationsAllComponent} from "./reservations-all.component";
+import {Route} from "../core/route.service";
 
-const routes: Routes = [
+const routes: Routes = Route.withShell( [
   { path: 'reservations-all', component: ReservationsAllComponent, data: { title: extract('Reservations - All') } }
-];
+]);
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
