2304258244529bfece59effef9c2edc3e845f953
Alec Clifford <alec.clifford@gmail.com>
2017-12-17 16:09:09 -0500

    Added remaining model fields to Reservation model, Added add and delete reservation calls

--- src/app/home/hop-api.service.ts
+++ src/app/home/hop-api.service.ts
@@ -2,20 +2,36 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 const url = 'https://ubk2kssfj5.execute-api.us-east-1.amazonaws.com/Dev/reservations';
+const headers = new HttpHeaders().set('x-api-key', 'Ai7RemrPPk96XGDz8pdHw1ZUeJZgDXGW18iispp9');
 
 export interface Reservation {
   propertyName: string
   checkInDate: string
   checkOutDate: string
   reservationSource: string
-  meetTime: string,
-  guestsNum: number,
-  apartment: boolean,
+  meetTime: string
+  guestsNum: number
+  apartment: boolean
   lastName: string
+  carrier: string
+  ddReturned: boolean
+  email: string
+  firstName: string
+  flightArrivalTime: string
+  flightNum: string
+  flightOrigCity: string
+  instructionsSent: boolean
+  notes: string
+  phoneNum: string
 }
 
 export interface GetReservationsResponse {
   Items: Reservation[]
+}
+
+export interface Response {
+  statusCode: string
+  body: string
 }
 
 @Injectable()
@@ -24,9 +40,17 @@ export class HopApiService {
   constructor(private httpClient: HttpClient) { }
 
   getReservations() {
-    const headers = new HttpHeaders().set('x-api-key', 'Ai7RemrPPk96XGDz8pdHw1ZUeJZgDXGW18iispp9');
     return this.httpClient.get<GetReservationsResponse>(url, {headers: headers })
   }
+
+  addReservation(reservation: Reservation) {
+    return this.httpClient.post<Response>(url, {headers: headers })
+  }
+
+  deleteReservation(reservation: Reservation) {
+    return this.httpClient.delete<Response>(url, {headers: headers })
+  }
+
   //
   // getRandomQuote(context: RandomQuoteContext): Observable<string> {
   //   return this.http.get(routes.quote(context), { cache: true })
