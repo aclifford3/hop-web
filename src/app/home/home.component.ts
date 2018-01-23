6d7353a28401dac7d596a29322e4aa255124ad0b
Alec Clifford <alec.clifford@gmail.com>
2018-01-22 19:26:02 -0500

    Added Palm Beach to Aruba group

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
     admin: ['All'],
     PalmBeach: ['Palm Beach'],
     Dushi: ['Dushi Tortuga', 'Dushi Iguana'],
-    ArubaClifford: ['Aruban Jewel', 'Blue Breeze', 'Casa Tranquila'],
+    ArubaClifford: ['Aruban Jewel', 'Blue Breeze', 'Casa Tranquila', 'Palm Beach'],
     US: ['Turnberry', 'Confederate']
   };
 
