ee6e8cbc5df495b051a56b67f81839c000b91132
Alec Clifford <alec.clifford@gmail.com>
2018-06-30 20:02:32 -0400

    Configure Prod to point at Prod API Gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import * as env from './.env.json';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.hop-web.com/Stage/reservations',
+  serverUrl: 'https://api.hop-web.com/Prod/reservations',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
